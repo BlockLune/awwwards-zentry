38bab7d70f0f70fb2c8627b24274a3c32133a07e
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-12-20 14:42:59 +0800

    feat: animated title

--- src/components/AnimatedTitle.tsx
+++ src/components/AnimatedTitle.tsx
@@ -1,20 +1,56 @@
+import { useEffect, useRef } from "react";
 import clsx from "clsx";
+import gsap from "gsap";
 
 function AnimatedTitle({
   title,
   containerClass,
 }: {
-  title: React.ReactNode;
+  title: string;
   containerClass: string;
 }) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    gsap.context(() => {
+      const titleAnimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "100 bottom",
+          end: "center bottom",
+          toggleActions: "play none none reverse",
+        },
+      });
+
+      titleAnimation.to(".animated-word", {
+        opacity: 1,
+        transform: "translate3d(0, 0, 0) rotateX(0deg) rotateY(0deg)",
+        ease: "power2.inOut",
+        stagger: 0.02,
+      });
+    }, containerRef);
+  }, []);
+
   return (
-    <div
-      className={clsx(
-        "mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]",
-        containerClass,
-      )}
-    >
-      {title}
+    <div ref={containerRef} className={clsx("animated-title", containerClass)}>
+      {title.split("<br />").map((line, index) => {
+        return (
+          <div
+            key={index}
+            className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+          >
+            {line.split(" ").map((word, i) => {
+              return (
+                <span
+                  key={i}
+                  className="animated-word"
+                  dangerouslySetInnerHTML={{ __html: word }}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
